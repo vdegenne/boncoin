4e9866273bf923a1c54651ad0261425cccef6114
vdegenne <vdegenne@gmail.com>
2021-12-26 19:31:31 +0100

    minified

--- src/front/rollup.config.js
+++ src/front/rollup.config.js
@@ -1,9 +1,11 @@
 import typescript from '@rollup/plugin-typescript'
 import nodeResolve from "@rollup/plugin-node-resolve";
 import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/front/app.ts',
   output: { file: 'app.js', format: 'esm' },
-  plugins: [nodeResolve(), typescript(), json()]
+  plugins: [nodeResolve(), typescript(), json(),
+    process.env.minify ? terser() : {}]
 }
--- src/server/rollup.config.js
+++ src/server/rollup.config.js
@@ -1,9 +1,11 @@
 import nodeResolve from "@rollup/plugin-node-resolve"
 import cjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'src/server/server.js',
   output: { file: 'server.js', format: 'cjs' },
-  plugins: [nodeResolve(), cjs(), json()]
+  plugins: [nodeResolve(), cjs(), json(),
+    process.env.minify ? terser() : {}]
 }
